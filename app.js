cf629ccd69fe8b8e3d9411a2458d11cacbf34611
PowerhouseG <kralicha6@gmail.com>
2022-06-04 21:58:20 +0300

    Added bcrypt and data checks

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const mongoose = require("mongoose");
 var User = require("./models/user");
+const bcrypt=require('bcryptjs');
+
 
 
 var indexRouter = require('./routes/index');
@@ -35,10 +37,15 @@ passport.use(
       if (!user) {
         return done(null, false, { message: "Incorrect username" });
       }
-      if (user.password !== password) {
-        return done(null, false, { message: "Incorrect password" });
-      }
-      return done(null, user);
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          // passwords match! log user in
+          return done(null, user);
+        } else {
+          // passwords do not match!
+          return done(null, false, { message: "Incorrect password" });
+        }
+      });
     });
   })
 );
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,8 @@ const path = require("path");
 const mongoose = require("mongoose");
 const passport = require("passport");
 var User = require("../models/user");
-
+const { body, validationResult } = require("express-validator");
+const bcrypt=require('bcryptjs');
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
@@ -16,17 +17,102 @@ router.get("/sign-in", function (req, res, next) {
 
 router.get("/sign-up", (req, res) => res.render("sign-up"));
 
-router.post("/sign-up", (req, res, next) => {
-  const user = new User({
-    username: req.body.email.split("@")[0],
-    password: req.body.password,
-  }).save((err) => {
-    if (err) {
-      return next(err);
+router.post("/sign-up",[
+  body("first_name")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("First name must be specified.")
+    .isAlphanumeric()
+    .withMessage("First name has non-alphanumeric characters."),
+  body("last_name")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage("Family name must be specified.")
+    .isAlphanumeric()
+    .withMessage("Last name has non-alphanumeric characters."),
+  body("email")
+    .trim()
+    .isLength({ min: 5 })
+    .escape()
+    .withMessage("Email must be specified.")
+    .isEmail()
+    .withMessage("Email must be valid"),
+  body("password")
+    .isLength({ min: 8 })
+    .withMessage("Password must be at least 8 characters")
+    .escape(),
+  body("confirm-password", "Passwords must match").custom((value, { req }) => {
+    return value === req.body.password;
+  }),
+
+  (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render form again with sanitized values/errors messages.
+      res.render("sign-up", {
+        title: "Sign up",
+        user_data: req.body,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      // Data from form is valid.
+      bcrypt.hash(req.body.password, 10, (err, hashedPassword) => {
+        if (err) {
+          next(err);
+        } else {
+          var user = new User({
+            first_name: req.body.first_name,
+            last_name: req.body.last_name,
+            email: req.body.email,
+            password: hashedPassword,
+            username: req.body.email.split("@")[0],
+          });
+          user.save(function (err) {
+            if (err) {
+              return next(err);
+            }
+            // Successful - redirect to new author record.
+            res.redirect("/sign-in");
+          });
+        }
+      });
     }
-    res.redirect("/sign-in");
-  });
-});
+  },
+]);
+
+// router.post("/sign-up", (req, res, next) => {
+//   // Extract the validation errors from a request.
+
+  
+//     // Data from form is valid.
+//     bcrypt.hash(req.body.password, 10, (err, hashedPassword) => {
+//       if (err) {
+//         next(err);
+//       } else {
+//         var user = new User({
+//           first_name: req.body.first_name,
+//           last_name: req.body.last_name,
+//           email: req.body.email,
+//           password: hashedPassword,
+//           username: req.body.email.split("@")[0],
+//         });
+//         user.save(function (err) {
+//           if (err) {
+//             return next(err);
+//           }
+//           // Successful - redirect to new author record.
+//           res.redirect("/sign-in");
+//         });
+//       }
+//     });
+  
+// });
+
 router.post(
   "/sign-in",
   passport.authenticate("local", {

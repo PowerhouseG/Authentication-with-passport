055b3ca2658612cd7765c35bd485f33f6c8702d0
PowerhouseG <kralicha6@gmail.com>
2022-06-06 20:50:23 +0300

    Added different roles

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 const passport = require("passport");
 var User = require("../models/user");
-var Message = require('../models/message')
+var Message = require("../models/message");
 const { body, validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
 
@@ -46,11 +46,9 @@ router.post("/sign-up", [
   }),
 
   (req, res, next) => {
-    // Extract the validation errors from a request.
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-      // There are errors. Render form again with sanitized values/errors messages.
       res.render("sign-up", {
         title: "Sign up",
         user_data: req.body,
@@ -58,7 +56,6 @@ router.post("/sign-up", [
       });
       return;
     } else {
-      // Data from form is valid.
       bcrypt.hash(req.body.password, 10, (err, hashedPassword) => {
         if (err) {
           next(err);
@@ -74,7 +71,6 @@ router.post("/sign-up", [
             if (err) {
               return next(err);
             }
-            // Successful - redirect to new author record.
             res.redirect("/sign-in");
           });
         }
@@ -100,32 +96,39 @@ router.get("/log-out", (req, res) => {
   });
 });
 router.get("/messages", function (req, res, next) {
-  Message.find().populate('user').exec((err,messages)=>{
-    if(err){
-      next(err);
-    }
-    else{
-      res.render("messages",{messages});
-    }
-  })
+  Message.find()
+    .populate("user")
+    .exec((err, messages) => {
+      if (err) {
+        next(err);
+      } else {
+        res.render("messages", { messages });
+      }
+    });
 });
 
-router.post("/messages",[
-  body('message',"Please Enter A Valid Message").trim().isLength({min:1}).escape(),
-  body('user_id').escape(),
-  body('title', "Please Enter A Valid Title").trim().isLength({min:1}).escape(),
-  
-   (req, res,next) => {
+router.post("/messages", [
+  body("message", "Please Enter A Valid Message")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("user_id").escape(),
+  body("title", "Please Enter A Valid Title")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+
+  (req, res, next) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
       res.render("messages", {
         message_title: req.body.title,
-        message_content:req.body.message,
+        message_content: req.body.message,
         errors: errors.array(),
       });
       return;
-    } else  {
+    } else {
       var message = new Message({
         message: req.body.message.trim(),
         title: req.body.title,
@@ -135,10 +138,73 @@ router.post("/messages",[
         if (err) {
           return next(err);
         }
-        // Successful - redirect to new author record.
         res.redirect("/messages");
       });
     }
-}]);
+  },
+]);
+
+router.post("/messages/:id", (req, res, next) => {
+  Message.findByIdAndRemove(req.params.id, function deleteInstance(err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/messages");
+  });
+});
+router.get("/join", function (req, res, next) {
+  res.render("join");
+});
+router.post("/join", [
+  body("join", "Please Enter A Valid Code").escape(),
+  body("admin", "Please Enter A Valid Admin Code").escape(),
+  function (req, res, next) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.render("join", {
+        join: req.body.join,
+        admin: req.body.admin,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      let success = "fail";
+      if (req.body.join == "join") {
+        success = "Verified";
+      }
+      if (req.body.admin == "admin") {
+        success = "Admin";
+      }
+      if (success !== "fail") {
+        var user = new User({
+          first_name: res.locals.currentUser.first_name,
+          last_name: res.locals.currentUser.last_name,
+          email: res.locals.currentUser.email,
+          password: res.locals.currentUser.password,
+          username: res.locals.currentUser.username,
+          status: success,
+          _id: res.locals.currentUser._id,
+        });
+        User.findByIdAndUpdate(
+          res.locals.currentUser._id,
+          user,
+          {},
+          (err, theuser) => {
+            if (err) {
+              return next(err);
+            }
+            res.render("join", {
+              success,
+            });
+          }
+        );
+      } else {
+        res.render("join", {
+          success,
+        });
+      }
+    }
+  },
+]);
 
 module.exports = router;
